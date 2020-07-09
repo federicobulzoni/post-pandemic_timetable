3194f56c4ed503dd9658eaa239cfe2ecaf224f8e
fbulzoni <federicobulzoni@yahoo.it>
2020-07-09 18:07:55 +0200

    Modifiche al filesystem e allo script per la generazione di test che ora li genera anche per ASP.

--- generateTests.py
+++ generateTests.py
@@ -1,23 +1,9 @@
 import os
 import random
-'''
-K = 2;
-capacity =
-  [60, 40, 45, 34, 60, 33, 34, 60];
-  
-N = 12;
-nStudents = [500, 500, 750, 1000, 400, 450, 600, 350, 390, 420, 450, 500];
-year = [1,2,3,2,3,2,1,1,3,2,3,1];
 
-D = 3;
-department = [1,2,3,3,2,2,3,1,3,1,2,3];
 
-requiredTime = [40, 55, 50, 45, 45, 55, 55, 40, 40, 50, 55, 45];
-'''
-
-# With K = 8 we consider 8*4 = 32 different classrooms.
 K = [2, 4, 6, 7, 8] 
-
+g = 4
 for k in K:
 	for it in range(20):
 		n = random.randint(2*k, 8*k)
@@ -29,16 +15,53 @@ for k in K:
 		department = [random.randint(1, d) for i in range(n)]
 		requiredTime = [random.randint(40, 60) for i in range(n)]
 
-		filename = "./test/K-{0}_{1}.dzn".format(k,it+1)
-		f = open(filename, 'w')
+		filename_mzn = "./mzn/test/K-{0}_{1}.dzn".format(k,it+1)
+		filename_lp = "./asp/test/K-{0}_{1}.lp".format(k,it+1)
+		f_mzn = open(filename_mzn, 'w')
+		f_lp = open(filename_lp, 'w')
 
-		f.write('K = {0};\n'.format(k))
-		f.write("capacity = {0};\n".format(capacity))
-		f.write('N = {0};\n'.format(n))
-		f.write('nStudents = {0};\n'.format(nStudents))
-		f.write('year = {0};\n'.format(year))
-		f.write('D = {0};\n'.format(d))
-		f.write('department = {0};\n'.format(department))
-		f.write('requiredTime = {0};\n'.format(requiredTime))
+		f_mzn.write('K = {0};\n'.format(k))
+		f_mzn.write("capacity = {0};\n".format(capacity))
+		f_mzn.write('N = {0};\n'.format(n))
+		f_mzn.write('nStudents = {0};\n'.format(nStudents))
+		f_mzn.write('year = {0};\n'.format(year))
+		f_mzn.write('D = {0};\n'.format(d))
+		f_mzn.write('department = {0};\n'.format(department))
+		f_mzn.write('requiredTime = {0};\n'.format(requiredTime))
 
-		f.close()
+		f_mzn.close()
+
+		f_lp.write('#const k = {0}.\n'.format(k))
+		for i in range(0,k*g):
+			f_lp.write('capacity({0},{1}).\n'.format(i+1, capacity[i]))
+
+		f_lp.write('#const n = {0}.\n'.format(n))
+		f_lp.write('#const d = {0}.\n'.format(d))
+		for i in range(0,n):
+			f_lp.write('nStudents({0},{1}).\n'.format(i+1, nStudents[i]))
+			f_lp.write('year({0},{1}).\n'.format(i+1, year[i]))
+			f_lp.write('department({0},{1}).\n'.format(i+1, department[i]))
+			f_lp.write('requiredTime({0},{1}).\n'.format(i+1, requiredTime[i]))
+
+		f_lp.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
